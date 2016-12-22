b332912ab2971b38f2b93323e638a40c75096918
Per Johan Karlsson <pkarlsson@rams.adp.vcu.edu>
2016-12-22 10:27:21 -0500

    added heroku server port config

--- devServer.js
+++ devServer.js
@@ -17,6 +17,10 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 //app.listen(7770, 'localhost', function(err) {
 //  if (err) {
 //    console.log(err);
